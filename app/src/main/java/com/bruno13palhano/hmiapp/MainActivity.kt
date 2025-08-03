cf1c9a04a48f41ec72e21ba55d8a38b75caa4a0a
bruno <bruno13palhano@gmail.com>
2025-08-02 23:17:56 -0300

    Add drawer menu

--- app/src/main/java/com/bruno13palhano/hmiapp/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/MainActivity.kt
@@ -6,14 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation3.runtime.rememberNavBackStack
 import com.bruno13palhano.hmiapp.ui.navigation.AppNavigation
+import com.bruno13palhano.hmiapp.ui.navigation.Dashboard
+import com.bruno13palhano.hmiapp.ui.navigation.DrawerMenu
 import com.bruno13palhano.hmiapp.ui.theme.HMIAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -30,7 +39,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HmiApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        AppNavigation(modifier = Modifier.padding(innerPadding))
+    val navBackStack = rememberNavBackStack(Dashboard)
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    var gesturesEnabled by rememberSaveable { mutableStateOf(true) }
+
+    DrawerMenu(
+        drawerState = drawerState,
+        navBackStack = navBackStack,
+        gesturesEnabled = gesturesEnabled
+    ) {
+        val scope = rememberCoroutineScope()
+
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            AppNavigation(
+                modifier = Modifier.padding(innerPadding),
+                navBackStack = navBackStack,
+                onGesturesEnable = { enable -> gesturesEnabled = enable },
+                onMenuIconClick = {
+                    scope.launch {
+                        drawerState.open()
+                    }
+                }
+            )
+        }
     }
 }
--- app/src/main/java/com/bruno13palhano/hmiapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/navigation/AppNavigation.kt
@@ -1,21 +1,50 @@
 package com.bruno13palhano.hmiapp.ui.navigation
 
+import android.content.res.Configuration
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
-import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
 import androidx.navigation3.ui.rememberSceneSetupNavEntryDecorator
+import com.bruno13palhano.hmiapp.R
 import com.bruno13palhano.hmiapp.ui.dashboard.DashboardScreen
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 @Composable
-fun AppNavigation(modifier: Modifier = Modifier) {
-    val backStack = rememberNavBackStack(Dashboard)
-
+fun AppNavigation(
+    modifier: Modifier = Modifier,
+    navBackStack: NavBackStack,
+    onGesturesEnable: (enable: Boolean) -> Unit,
+    onMenuIconClick: () -> Unit
+) {
     NavDisplay(
         modifier = modifier,
         entryDecorators = listOf(
@@ -23,12 +52,16 @@ fun AppNavigation(modifier: Modifier = Modifier) {
             rememberSavedStateNavEntryDecorator(),
             rememberViewModelStoreNavEntryDecorator()
         ),
-        backStack = backStack,
-        onBack = { backStack.removeLastOrNull() }
+        backStack = navBackStack,
+        onBack = { navBackStack.removeLastOrNull() }
     ) { key ->
         when (key) {
             Dashboard -> NavEntry(key) { entry ->
                 DashboardScreen()
+            }
+
+            Settings -> NavEntry(key) { entry ->
+
             }
             else -> {
                 error("Unknown route: $key")
@@ -39,3 +72,78 @@ fun AppNavigation(modifier: Modifier = Modifier) {
 
 @Serializable
 data object Dashboard : NavKey
+
+@Serializable
+data object Settings :  NavKey
+
+sealed class Screen(
+    val key: NavKey,
+    val icon: ImageVector,
+    @StringRes val resourceId: Int,
+) {
+    data object DashboardScreen : Screen(
+        key = Dashboard,
+        icon = Icons.Outlined.Home,
+        resourceId = R.string.app_name
+    )
+
+    data object SettingsScreen: Screen(
+        key = Settings,
+        icon = Icons.Outlined.Settings,
+        resourceId = R.string.app_name
+    )
+}
+
+@Composable
+fun DrawerMenu(
+    drawerState: DrawerState,
+    navBackStack: NavBackStack,
+    gesturesEnabled: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val orientation = LocalConfiguration.current.orientation
+    val items = listOf(Screen.DashboardScreen, Screen.SettingsScreen)
+    val scope = rememberCoroutineScope()
+    val currentDestination = navBackStack.lastOrNull()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = gesturesEnabled,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    Modifier.fillMaxWidth(.78F)
+                } else {
+                    Modifier
+                },
+            ) {
+                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                    Spacer(modifier = Modifier.height(12.dp))
+                    items.forEach { screen ->
+                        NavigationDrawerItem(
+                            shape = RoundedCornerShape(0, 50, 50, 0),
+                            icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+                            label = { Text(text = stringResource(id = screen.resourceId)) },
+                            selected = currentDestination == screen.key,
+                            onClick = {
+                                if (currentDestination != screen.key) {
+                                    navBackStack.add(element = screen.key)
+                                    if (currentDestination != Dashboard) {
+                                        navBackStack.remove(element = currentDestination)
+                                    }
+                                }
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            modifier = Modifier
+                                .padding(top = 4.dp, bottom = 4.dp, end = 8.dp)
+                                .height(52.dp),
+                        )
+                    }
+                }
+            }
+        },
+        content = content
+    )
+}
