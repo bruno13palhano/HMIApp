7919c78707d36356e0e230b39ece428923e8ea5f
bruno <bruno13palhano@gmail.com>
2025-08-05 15:38:08 -0300

    Remove unused import

--- app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetCanvas.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetCanvas.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.bruno13palhano.core.model.Widget
 
 @Composable
