417a5b41d2e2af2cd641444f5a80a835a93d9bfb
bruno <bruno13palhano@gmail.com>
2025-08-05 15:37:04 -0300

    Refactor widget toolbox

--- app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetToolbox.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetToolbox.kt
@@ -4,33 +4,116 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bruno13palhano.core.model.WidgetType
+import com.bruno13palhano.hmiapp.R
+import com.bruno13palhano.hmiapp.ui.theme.HMIAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WidgetToolbox(onAdd: (WidgetType) -> Unit) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .background(Color.White, RoundedCornerShape(8.dp))
-            .border(1.dp, Color.Gray)
-            .padding(8.dp)
-    ) {
-        WidgetType.entries.forEach { type ->
-            Button(
-                onClick = { onAdd(type) },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+fun WidgetToolbox(
+    expanded: Boolean,
+    onAdd: (WidgetType) -> Unit,
+    onExpandedClick: () -> Unit
+) {
+    Column {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            IconButton(
+                onClick = onExpandedClick,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(Alignment.Center)
             ) {
-                Text(text = "+ ${type.name}")
+                var contentDescription = stringResource(id = R.string.hide_widget_toolbox)
+                var icon = Icons.Outlined.ExpandLess
+
+                if (expanded) {
+                    icon = Icons.Outlined.ExpandMore
+                    contentDescription = stringResource(id = R.string.hide_widget_toolbox)
+                }
+
+                Icon(
+                    imageVector = icon,
+                    contentDescription = contentDescription,
+                    tint = MaterialTheme.colorScheme.onSecondary
+                )
             }
+        }
+        if (expanded) {
+            Column(
+                modifier = Modifier
+                    .offset(y = (-32).dp)
+                    .padding(16.dp)
+                    .background(
+                        MaterialTheme.colorScheme.background,
+                        RoundedCornerShape(5)
+                    )
+                    .border(1.dp, MaterialTheme.colorScheme.secondary, RoundedCornerShape(5))
+                    .padding(8.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                WidgetType.entries.forEach { type ->
+                    Button(
+                        onClick = { onAdd(type) },
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+                    ) {
+                        Text(text = "+ ${type.name}")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun WidgetToolboxPreview() {
+    HMIAppTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
+            WidgetToolbox(
+                expanded = true,
+                onAdd = {
+                    listOf(WidgetType.TEXT, WidgetType.BUTTON, WidgetType.SWITCH)
+                },
+                onExpandedClick = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun WidgetToolboxNotExpandPreview() {
+    HMIAppTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
+            WidgetToolbox(
+                expanded = false,
+                onAdd = {
+                    listOf(WidgetType.TEXT, WidgetType.BUTTON, WidgetType.SWITCH)
+                },
+                onExpandedClick = {}
+            )
         }
     }
 }
