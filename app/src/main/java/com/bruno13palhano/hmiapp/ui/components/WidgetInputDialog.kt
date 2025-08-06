e26871d4b72a51995fd4b8688ad12afdb2739b79
bruno <bruno13palhano@gmail.com>
2025-08-06 14:22:33 -0300

    Change from TextButton to Button and adjust padding

--- app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetInputDialog.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetInputDialog.kt
@@ -1,20 +1,23 @@
 package com.bruno13palhano.hmiapp.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -38,33 +41,44 @@ fun WidgetInputDialog(
         ) {
             Column(modifier = Modifier.padding(16.dp)) {
                 Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
                     text = stringResource(id = R.string.widget_configuration),
                 )
                 Spacer(modifier = Modifier.height(24.dp))
                 CustomTextField(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .fillMaxWidth(),
                     value = label,
                     onValueChange = onLabelChange,
                     label = stringResource(id = R.string.label),
                     placeholder = stringResource(id = R.string.enter_label)
                 )
                 CustomTextField(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .fillMaxWidth(),
                     value = endpoint,
                     onValueChange = onEndpointChange,
                     label = stringResource(id = R.string.endpoint),
                     placeholder = stringResource(id = R.string.enter_endpoint)
                 )
 
-                Row {
-                    TextButton(
+                Row(
+                    modifier = Modifier
+                        .padding(top = 16.dp, end = 8.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Button(
                         onClick = onDismissRequest,
-                        modifier = Modifier
+                        modifier = Modifier.padding(end = 4.dp)
                     ) {
                         Text(text = stringResource(id = R.string.cancel))
                     }
-
-                    TextButton(
+                    Button(
                         onClick = onConfirm,
-                        modifier = Modifier
+                        modifier = Modifier.padding(start = 4.dp)
                     ) {
                         Text(text = stringResource(id = R.string.ok))
                     }
