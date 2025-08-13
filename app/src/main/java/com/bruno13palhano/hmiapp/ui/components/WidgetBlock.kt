90191b69261a5fc02b071a12518ddc4b62f0de5f
bruno <bruno13palhano@gmail.com>
2025-08-12 21:45:09 -0300

    Make WidgetBlock a part of WidgetRender to simplify widget rendering logic

--- app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetBlock.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetBlock.kt
@@ -3,15 +3,20 @@ package com.bruno13palhano.hmiapp.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -26,11 +32,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bruno13palhano.core.model.DataSource
 import com.bruno13palhano.core.model.Widget
 import com.bruno13palhano.core.model.WidgetType
@@ -42,12 +48,20 @@ import kotlin.math.roundToInt
 fun WidgetBlock(
     widget: Widget,
     onMove: (x: Float, y: Float) -> Unit,
-    onRemove: () -> Unit
+    onEdit: () -> Unit,
+    onRemove: () -> Unit,
+    content: @Composable ColumnScope.() -> Unit
 ) {
     var offsetX by remember { mutableFloatStateOf(widget.x) }
     var offsetY by remember { mutableFloatStateOf(widget.y) }
 
-    Box(
+    var expandMenu by remember { mutableStateOf(false) }
+    val items = mapOf(
+        MenuOptions.EDIT to stringResource(id = R.string.edit),
+        MenuOptions.REMOVE to stringResource(id = R.string.remove)
+    )
+
+    Column(
         modifier = Modifier
             .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
             .pointerInput(Unit) {
@@ -65,45 +79,68 @@ fun WidgetBlock(
                 shape = RoundedCornerShape(8.dp)
             )
             .border(1.dp, MaterialTheme.colorScheme.secondary),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row (
+            modifier = Modifier.width(widget.width.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
             Text(
-                modifier = Modifier.padding(horizontal = 8.dp),
-                text = widget.label,
+                modifier = Modifier.padding(horizontal = 8.dp).weight(2f),
+                text = getWidgetTypeName(widgetType = widget.type),
                 fontWeight = FontWeight.Bold,
                 maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center
             )
-
-            Text(
-                modifier = Modifier.padding(horizontal = 8.dp),
-                text = when (val ds = widget.dataSource) {
-                    is DataSource.MQTT -> "MQTT: ${ds.topic}"
-                    is DataSource.HTTP -> "HTTP: ${ds.url}"
-                },
-                fontSize = 12.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            Text(
-                modifier = Modifier.padding(horizontal = 8.dp),
-                text = "${stringResource(id = R.string.value)}: ${widget.value}",
-                fontSize = 12.sp,
-                color = MaterialTheme.colorScheme.primary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            IconButton(onClick = onRemove) {
+            IconButton(
+                modifier = Modifier.weight(1f),
+                onClick = { expandMenu = !expandMenu },
+            ) {
                 Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = stringResource(id = R.string.remove)
+                    imageVector = Icons.Outlined.MoreVert,
+                    contentDescription = null
+                )
+
+                VertMenu(
+                    items = items,
+                    expanded = expandMenu,
+                    onDismissRequest = { expandMenu = !expandMenu},
+                    onItemClick = { item ->
+                        when (item) {
+                            MenuOptions.EDIT -> onEdit()
+                            MenuOptions.REMOVE -> onRemove()
+                        }
+                    }
                 )
             }
         }
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            content = content
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                .fillMaxWidth(),
+            text = widget.label,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Center
+        )
     }
+}
+
+private enum class MenuOptions {
+    EDIT,
+    REMOVE
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -114,25 +151,14 @@ private fun WidgetBlockPreview() {
             WidgetBlock(
                 widget = Widget(
                     type = WidgetType.SWITCH,
-                    label = "Switch",
+                    label = "M1",
                     dataSource = DataSource.MQTT(topic = "test/topic"),
                     value = "ON"
                 ),
                 onMove = { _, _ -> },
-                onRemove = { }
-            )
-
-            WidgetBlock(
-                widget = Widget(
-                    type = WidgetType.TEXT,
-                    label = "Gauss",
-                    dataSource = DataSource.HTTP(url = "http://www.test.com/test"),
-                    value = "120.2",
-                    x = 250f,
-                    y = 500f
-                ),
-                onMove = { _, _ -> },
-                onRemove = { }
+                onEdit = { },
+                onRemove = { },
+                content = {}
             )
         }
     }
--- app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetCanvas.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/components/WidgetCanvas.kt
@@ -6,13 +6,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bruno13palhano.core.model.DataSource
 import com.bruno13palhano.core.model.Widget
+import com.bruno13palhano.core.model.WidgetType
+import com.bruno13palhano.hmiapp.ui.theme.HMIAppTheme
 
 @Composable
 fun WidgetCanvas(
     widgets: List<Widget>,
     onMove: (id: String, x: Float, y: Float) -> Unit,
-    onRemove: (String) -> Unit,
+    onEdit: (id: String) -> Unit,
+    onRemove: (id: String) -> Unit,
+    onEvent: (event: WidgetEvent) -> Unit
 ) {
     Box(
         modifier = Modifier
@@ -20,11 +26,92 @@ fun WidgetCanvas(
             .background(MaterialTheme.colorScheme.onSecondaryContainer)
     ) {
         widgets.forEach { widget ->
-            WidgetBlock(
+            WidgetRenderer(
                 widget = widget,
                 onMove = { x, y -> onMove(widget.id, x, y) },
-                onRemove = { onRemove(widget.id) }
+                onEdit = { onEdit(widget.id) },
+                onRemove = { onRemove(widget.id) },
+                onEvent = onEvent,
             )
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun WidgetCanvasPreview() {
+    HMIAppTheme {
+        WidgetCanvas(
+            widgets = listOf(
+                Widget(
+                    type = WidgetType.GAUGE,
+                    label = "V1",
+                    dataSource = DataSource.MQTT(topic = "gauge/v1"),
+                    value = "200",
+                    x = 20f,
+                    y = 20f
+                ),
+                Widget(
+                    type = WidgetType.TOGGLE_BUTTON,
+                    label = "T1",
+                    dataSource = DataSource.MQTT(topic = "toggle/t1"),
+                    value = "true",
+                    x = 580f,
+                    y = 20f
+                ),
+                Widget(
+                    type = WidgetType.SWITCH,
+                    label = "S1",
+                    dataSource = DataSource.MQTT(topic = "switch/s1"),
+                    value = "true",
+                    x = 20f,
+                    y = 500f
+                ),
+                Widget(
+                    type = WidgetType.BUTTON,
+                    label = "B1",
+                    dataSource = DataSource.MQTT(topic = "button/b1"),
+                    value = "",
+                    x = 580f,
+                    y = 500f
+                ),
+                Widget(
+                    type = WidgetType.PROGRESS_BAR,
+                    label = "P1",
+                    dataSource = DataSource.MQTT(topic = "progress/p1"),
+                    value = ".63",
+                    x = 20f,
+                    y = 1000f
+                ),
+                Widget(
+                    type = WidgetType.SLIDER,
+                    label = "L1",
+                    dataSource = DataSource.MQTT(topic = "test/topic"),
+                    value = "40.7",
+                    x = 580f,
+                    y = 1000f
+                ),
+                Widget(
+                    type = WidgetType.LED_INDICATOR,
+                    label = "LED1",
+                    dataSource = DataSource.MQTT(topic = "test/topic"),
+                    value = "WARN",
+                    x = 20f,
+                    y = 1500f
+                ),
+                Widget(
+                    type = WidgetType.INPUT_FIELD,
+                    label = "M1",
+                    dataSource = DataSource.MQTT(topic = "test/topic"),
+                    value = "ON",
+                    x = 580f,
+                    y = 1500f
+                )
+            ),
+            { _, _, _ -> },
+            {},
+            {},
+            onEvent = {}
+        )
     }
 }
