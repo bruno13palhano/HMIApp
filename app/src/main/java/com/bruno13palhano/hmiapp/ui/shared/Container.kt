ddce911eae35b2c17b7404727aa744c23f6e4441
bruno <bruno13palhano@gmail.com>
2025-08-20 20:34:59 -0300

    Refactor Container

--- app/src/main/java/com/bruno13palhano/hmiapp/ui/shared/Container.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/shared/Container.kt
@@ -1,35 +1,87 @@
 package com.bruno13palhano.hmiapp.ui.shared
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 
 class Container<STATE, EFFECT>(
     initialSTATE: STATE,
-    private val scope: CoroutineScope,
+    private val scope: CoroutineScope = CoroutineScope(
+        SupervisorJob() + Dispatchers.Main.immediate
+    ),
+    private val onError: (Throwable) -> Unit = { it.printStackTrace() }
 ) {
     private val _state: MutableStateFlow<STATE> = MutableStateFlow(initialSTATE)
     val state: StateFlow<STATE> = _state.asStateFlow()
 
-    private val _sideEffect = Channel<EFFECT>(Channel.CONFLATED)
-    val sideEffect: Flow<EFFECT> = _sideEffect.receiveAsFlow()
+    private val mutex = Mutex()
+
+    private val _sideEffect = MutableSharedFlow<EFFECT>(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val sideEffect: Flow<EFFECT> = _sideEffect.asSharedFlow()
 
     fun intent(transform: suspend Container<STATE, EFFECT>.() -> Unit) {
         scope.launch {
-            this@Container.transform()
+            try {
+                this@Container.transform()
+            } catch (e: Exception) {
+                onError(e)
+            }
+        }
+    }
+
+    /**
+     * Variation to make possible to choice the coroutine dispatcher.
+     */
+    fun intent(
+        dispatcher: CoroutineDispatcher,
+        transform: suspend Container<STATE, EFFECT>.() -> Unit
+    ) {
+        scope.launch(dispatcher) {
+            try {
+                this@Container.transform()
+            } catch (e: Exception) {
+                onError(e)
+            }
+        }
+    }
+
+    suspend fun intentSync(transform: suspend Container<STATE, EFFECT>.() -> Unit) {
+        try {
+            this.transform()
+        } catch (e: Exception) {
+            onError(e)
         }
     }
 
     suspend fun reduce(reducer: suspend STATE.() -> STATE) {
-        _state.value = _state.value.reducer()
+        withContext(Dispatchers.Main.immediate) {
+            mutex.withLock {
+                _state.value = _state.value.reducer()
+            }
+        }
     }
 
     suspend fun postSideEffect(effect: EFFECT) {
-        _sideEffect.send(effect)
+        withContext(Dispatchers.Main.immediate) {
+            mutex.withLock {
+                _sideEffect.emit(effect)
+            }
+        }
     }
 }
