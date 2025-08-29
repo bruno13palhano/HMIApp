23dd884a3d3d99bea4921a17bdf20bd11fc2417a
bruno <bruno13palhano@gmail.com>
2025-08-29 10:22:53 -0300

    Move LayoutConfig to a file

---
+++ core/src/main/java/com/bruno13palhano/core/data/configuration/LayoutConfig.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.core.data.configuration
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LayoutConfig(
+    val environment: EnvironmentConfig,
+    val widgets: List<WidgetConfig>
+)
