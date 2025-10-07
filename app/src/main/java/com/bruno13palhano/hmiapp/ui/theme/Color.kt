3a31e7a51b8fe2320a161e6e63f2e9bc0f2184d9
bruno <bruno13palhano@gmail.com>
2025-10-07 11:15:55 -0300

    Added custom theme

--- app/src/main/java/com/bruno13palhano/hmiapp/ui/theme/Color.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/theme/Color.kt
@@ -2,10 +2,52 @@ package com.bruno13palhano.hmiapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_primary = Color(0xFF00796B)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF48A999)
+val md_theme_light_onPrimaryContainer = Color(0xFF00332D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_light_secondary = Color(0xFF455A64)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFCFD8DC)
+val md_theme_light_onSecondaryContainer = Color(0xFF1C313A)
+
+val md_theme_light_tertiary = Color(0xFFFFB300)
+val md_theme_light_onTertiary = Color(0xFF3E2F00)
+val md_theme_light_tertiaryContainer = Color(0xFFFFE082)
+val md_theme_light_onTertiaryContainer = Color(0xFF231B00)
+
+val md_theme_light_error = Color(0xFFD32F2F)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+
+val md_theme_light_background = Color(0xFFF4F7F6)
+val md_theme_light_onBackground = Color(0xFF1C1B1F)
+val md_theme_light_surface = Color(0xFFFFFFFF)
+val md_theme_light_onSurface = Color(0xFF1C1B1F)
+val md_theme_light_outline = Color(0xFFB0BEC5)
+
+
+// ======== DARK THEME COLORS ========
+val md_theme_dark_primary = Color(0xFF48A999)
+val md_theme_dark_onPrimary = Color(0xFF00332D)
+val md_theme_dark_primaryContainer = Color(0xFF005046)
+val md_theme_dark_onPrimaryContainer = Color(0xFF8DF2DE)
+
+val md_theme_dark_secondary = Color(0xFFB0BEC5)
+val md_theme_dark_onSecondary = Color(0xFF263238)
+val md_theme_dark_secondaryContainer = Color(0xFF37474F)
+val md_theme_dark_onSecondaryContainer = Color(0xFFDDE5E8)
+
+val md_theme_dark_tertiary = Color(0xFFFFC107)
+val md_theme_dark_onTertiary = Color(0xFF2C2100)
+val md_theme_dark_tertiaryContainer = Color(0xFF5A4300)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFE08F)
+
+val md_theme_dark_error = Color(0xFFF44336)
+val md_theme_dark_onError = Color(0xFF690005)
+
+val md_theme_dark_background = Color(0xFF121212)
+val md_theme_dark_onBackground = Color(0xFFE0E0E0)
+val md_theme_dark_surface = Color(0xFF1E1E1E)
+val md_theme_dark_onSurface = Color(0xFFE0E0E0)
+val md_theme_dark_outline = Color(0xFF90A4AE)
--- app/src/main/java/com/bruno13palhano/hmiapp/ui/theme/Theme.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bruno13palhano.hmiapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,26 +10,48 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    secondaryContainer = md_theme_light_secondaryContainer,
+    onSecondaryContainer = md_theme_light_onSecondaryContainer,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    onTertiaryContainer = md_theme_light_onTertiaryContainer,
+    tertiary = md_theme_light_tertiary,
+    onTertiary = md_theme_light_onTertiary,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    error = md_theme_light_error,
+    onError = md_theme_light_onError,
+    outline = md_theme_light_outline
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    error = md_theme_dark_error,
+    onError = md_theme_dark_onError,
+    outline = md_theme_dark_outline
 )
 
 @Composable
