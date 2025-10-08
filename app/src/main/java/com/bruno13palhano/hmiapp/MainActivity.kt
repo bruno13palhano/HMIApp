66cff95fa14de0848c1042b80b95ffcefdf1c91b
bruno <bruno13palhano@gmail.com>
2025-10-08 12:55:50 -0300

    Added app icon

--- app/src/main/java/com/bruno13palhano/hmiapp/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/hmiapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bruno13palhano.hmiapp.ui.navigation.AppNavigation
 import com.bruno13palhano.hmiapp.ui.theme.HMIAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             HMIAppTheme {
